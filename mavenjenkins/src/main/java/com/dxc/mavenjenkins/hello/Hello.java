c6d75faf247cbd31622f06017bb7579994506d48
dboppana <dboppana@host.docker.internal>
2020-09-26 02:01:57 -0700

    huhhuuu

---
+++ mavenjenkins/src/main/java/com/dxc/mavenjenkins/hello/Hello.java
@@ -0,0 +1,13 @@
+package com.dxc.mavenjenkins.hello;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Jenkins");
+		
+		System.out.println("praveen is a good guy");
+		
+	}
+
+}
